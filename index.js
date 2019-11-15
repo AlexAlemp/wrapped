387756ceac495bbc689aa83791153672a5cbcd37
Aleksandar Alempijevic <aleksandar.alempijevic@alterset.net>
2019-11-15 12:25:51 +0100

    Init

---
+++ index.js
@@ -0,0 +1,11 @@
+const inputElement = document.getElementById("input");
+const files = document.getElementById("files");
+inputElement.addEventListener("click", handleFiles, false);
+console.log(files);
+function handleFiles(e) {
+    e.preventDefault();
+    window.ReactNativeWebView.postMessage('default prevented');
+}
+document.addEventListener("message", function(event) {
+    alert(event);
+});
